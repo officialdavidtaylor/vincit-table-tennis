330456e62657fabb7f4134166385e1e32694accd
officialdavidtaylor <officialdavidtaylor@gmail.com>
2022-11-08 13:48:19 -0800

    feat: add react-hook-form to project

--- src/app/(webApp)/register/page.tsx
+++ src/app/(webApp)/register/page.tsx
@@ -4,7 +4,7 @@ import Input from "@components/Form/Input";
 export default function Home() {
   return (
     <>
-      <form>
+      <form className="flex flex-col gap-4">
         <Input title="Player Name" type="text" placeholder="First Last " />
         <Input
           title="Player Email"
