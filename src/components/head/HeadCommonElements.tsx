700c063cca996ff8e442891b439a8d87b2987519
Joseph Bravo <arks.josephbravo@gmail.com>
2022-11-10 13:05:40 -0800

    Move common elements and app icons into their own component

---
+++ src/components/head/HeadCommonElements.tsx
@@ -0,0 +1,10 @@
+export default function HeadCommonElements() {
+  return (
+    <>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width"
+      />
+    </>
+  );
+}
---
+++ src/components/head/HeadIcons.tsx
@@ -0,0 +1,23 @@
+export default function HeadIcons() {
+  return (
+    <>
+      <link rel="apple-touch-icon" href="touch-icon-ipad-retina.png"></link>
+      {/* Apple iPhone High Resolution */}
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="touch-icon-iphone-retina.png"
+      />
+      {/* Apple iPad High Resolution */}
+      <link
+        rel="apple-touch-icon"
+        sizes="167x167"
+        href="touch-icon-ipad-retina.png"
+      />
+      {/* Android Devices High Resolution */}
+      <link rel="icon" sizes="192x192" href="touch-icon-hd.png" />
+      {/* Android Devices Normal Resolution */}
+      <link rel="icon" sizes="128x128" href="touch-icon.png" />
+    </>
+  );
+}
